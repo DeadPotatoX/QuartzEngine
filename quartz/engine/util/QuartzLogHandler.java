1f74c22af83d44a7d129b277085bab45655f2ccc
DeadPotatoX <zacrox06@gmail.com>
2014-04-12 18:42:14 +1200

    Added a un-used log handler

---
+++ quartz/engine/util/QuartzLogHandler.java
@@ -0,0 +1,23 @@
+package quartz.engine.util;
+
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+
+/**
+ * This is just here
+ */
+public class QuartzLogHandler extends Handler {
+	public String[] logs;
+
+	public void publish(LogRecord record) {
+		
+	}
+
+	public void flush() {
+		
+	}
+
+	public void close() throws SecurityException {
+		
+	}
+}
